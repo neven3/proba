3345c7dc4ad88f5adce966abb81afc1a4992c820
neven3 <61207254+neven3@users.noreply.github.com>
2020-08-27 12:24:14 +0200

    Updates

---
+++ precache-manifest.762da1a32c0cab4334ac679e0324b889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b20b244cd3877d43183ded01160fea",
+    "url": "/proba/index.html"
+  },
+  {
+    "revision": "579ce29aff4914645288",
+    "url": "/proba/static/css/main.b5e80b34.chunk.css"
+  },
+  {
+    "revision": "696fe1764349a562ab75",
+    "url": "/proba/static/js/2.300a890c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/proba/static/js/2.300a890c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "579ce29aff4914645288",
+    "url": "/proba/static/js/main.a88ce075.chunk.js"
+  },
+  {
+    "revision": "c04fd5b8f44fc9a1792f",
+    "url": "/proba/static/js/runtime-main.8def31dd.js"
+  }
+]);
